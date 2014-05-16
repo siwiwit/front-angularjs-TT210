c66338f7c4857853f2a1c3605ec6c7caaec8aad3
telosystoolsdemo <telosystoolsdemo@gmail.com>
2014-05-16 14:08:23 +0200

    random values for key attributes

--- classes/Tools.java
+++ classes/Tools.java
@@ -6,43 +6,22 @@ import java.util.LinkedList;
 import java.util.Map;
 
 import org.telosys.tools.generator.context.EntityInContext;
-import org.telosys.tools.generator.context.JavaBeanClassAttribute;
-import org.telosys.tools.generator.context.JavaBeanClassForeignKey;
-import org.telosys.tools.generator.context.JavaBeanClassForeignKeyColumn;
-import org.telosys.tools.generator.context.LinkInContext;
-import org.telosys.tools.generator.GeneratorException;
+import org.telosys.tools.generator.context.AttributeInContext;
 
 public class Tools {
 	
-
-	private void addReferencedEntityTypes( EntityInContext entity, JavaBeanClassAttribute field, List<String> referencedEntityTypes ) 
-	throws GeneratorException{
-		for( LinkInContext link : entity.getLinks()  ) {
-			if( link.isOwningSide() && link.hasJoinColumns() ) {
-				for( String joinColumn : link.getJoinColumns() ) {
-					if( joinColumn.equals(field.getDatabaseName() ) ) {						
-						String referencedEntityType = link.getTargetEntitySimpleType() ;
-						if ( referencedEntityTypes.contains(referencedEntityType) == false ) {
-							// TODO : unique ? (HashMap)
-							referencedEntityTypes.add( link.getTargetEntitySimpleType() );
-						}
-					}
-				}
-			}
+	public List<Object> randomKeyAttributesValues(EntityInContext entity) {
+		List<AttributeInContext> keys = entity.getKeyAttributes();
+		if(keys == null) {
+			return new ArrayList<Object>();
 		}
+		List<Object> values = new ArrayList<Object>();
+		int i=1;
+		for(AttributeInContext key : keys) {
+			values.add(String.valueOf(i));
+			i++;
+		}
+		return values;
 	}
 	
-	public List<String> referencedEntityTypes( EntityInContext entity, JavaBeanClassAttribute field) throws GeneratorException {
-		List<String> referencedEntityTypes = new LinkedList<String>();
-		addReferencedEntityTypes( entity, field, referencedEntityTypes );
-		return referencedEntityTypes ;
-	}
-
-	public List<String> referencedEntityTypes( EntityInContext entity, List<JavaBeanClassAttribute> fields) throws GeneratorException {
-		List<String> referencedEntityTypes = new LinkedList<String>();
-		for ( JavaBeanClassAttribute field : fields ) {
-			addReferencedEntityTypes( entity, field, referencedEntityTypes );
-		}
-		return referencedEntityTypes ;
-	}
 }
